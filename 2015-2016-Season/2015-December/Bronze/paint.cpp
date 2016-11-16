ba1b1aed3c429a394b4fbe67b46a67329b5cddf4
CLDP <muyunhai@gmail.com>
2016-11-16 17:35:30 -0500

    Update paint.cpp

--- 2015-2016-Season/2015-December/Bronze/paint.cpp
+++ 2015-2016-Season/2015-December/Bronze/paint.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 int main() {
-	freopen("paint.in", "r", stdin);
-	freopen("paint.out", "w", stdout);
-	int a, b, c, d;
-	cin >> a >> b >> c >> d;
-	if (b <= c || d <= a) {
-		cout << b - a + d - c << endl;
-	} else {
-		cout << max(b, d) - min(a, c) << endl;
-	}
-	return 0;
+    freopen("paint.in", "r", stdin);
+    freopen("paint.out", "w", stdout);
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (b <= c || d <= a) {
+        cout << b - a + d - c << endl;
+    } else {
+        cout << max(b, d) - min(a, c) << endl;
+    }
+    return 0;
 }
