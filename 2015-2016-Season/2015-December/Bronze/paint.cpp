7b3aa395707be24c284ddb94301ae08ffffe4c08
CLDP <muyunhai@gmail.com>
2016-11-16 17:06:10 -0500

    Create paint.cpp

---
+++ 2015-2016-Season/2015-December/Bronze/paint.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	freopen("paint.in", "r", stdin);
+	freopen("paint.out", "w", stdout);
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	if (b <= c || d <= a) {
+		cout << b - a + d - c << endl;
+	} else {
+		cout << max(b, d) - min(a, c) << endl;
+	}
+	return 0;
+}
